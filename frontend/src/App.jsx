00beb3af5a8a44a4ccebf8e4b780656843072df5
Nahuel Uboldi <nahueluboldi@gmail.com>
2022-12-30 12:57:36 -0300

    header and footer components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
