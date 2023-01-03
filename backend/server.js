f4416355e65694424f8a196bbfd362e059f9ce15
Nahuel Uboldi <nahueluboldi@gmail.com>
2023-01-03 19:05:54 -0300

    mongoose schemas added & seeder script failed

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 app.use(cors());
@@ -22,5 +26,7 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   5000,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
