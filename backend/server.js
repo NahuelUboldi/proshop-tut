7d81284961d64da0e199073fe2601361a3d99da3
nahueluboldi <nahueluboldi@gmail.com>
2023-01-03 17:35:20 -0300

    moongose setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const products = require('./data/products');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
 dotenv.config();
 
