83400440461c2f8690fe41811f65ccc30c63f956
Nahuel Uboldi <nahueluboldi@gmail.com>
2023-02-09 21:13:18 -0300

    user auth

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import colors from 'colors';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errorHAndler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -14,11 +15,14 @@ connectDB();
 const app = express();
 app.use(cors());
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHAndler);
